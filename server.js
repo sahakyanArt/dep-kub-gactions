0c518e8031b55c49823827145dad1507fb9afca6
Artur <sahakyan.art@outlook.com>
2025-09-18 15:06:04 +0400

    debug check

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 'use strict';
 
 const express = require('express');
+
+// Constants
 const PORT = 3000;
 const HOST = '0.0.0.0';
 
+// App
 const app = express();
 app.get('/', (req, res) => {
   res.send('Hello World');
